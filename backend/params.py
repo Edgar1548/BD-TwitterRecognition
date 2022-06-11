ddf442b83a394c056f0ba4cf3c92320feca83367
Edgar_Chacon_1548 <70813796+Edgar1548@users.noreply.github.com>
2022-06-11 00:53:31 -0500

    Add files via upload

---
+++ backend/params.py
@@ -0,0 +1,7 @@
+static_path_folder = "data_static/data_elecciones/"
+static_path_clean = "data_static/data_elecciones_clean/"
+index_path = "data_static/index_inv_blocks/indexinv.json"
+norm_path = "data_static/norm_static/norm.json"
+twitter_path = "data_twitter/"
+
+stoplist_file = "stoplist.txt"
